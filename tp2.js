7bff9a6b3c59ffb798f96f2048f0107cf25dda0f
EtienneHoarau <76174089+EtienneHoarau@users.noreply.github.com>
2022-06-02 11:30:16 +0200

    Add files via upload

---
+++ tp2.js
@@ -0,0 +1,75 @@
+var listemots= new Array("interstellar","joker","batman","pulp fiction","sonic","the exorcist","shrek","scream","ghostBuster","avatar","alerte rouge","the mask","le roi lion","halloween","maman j ai rate l avion")
+var motactuel;
+var motDeviner= new Array();
+var vie;
+var lettreUtiliser;
+
+function afficher() {
+    temp ="";
+    for (let i = 0; i < motDeviner.length; i++) {
+        temp=temp+motDeviner[i]+" ";        
+    }
+    document.getElementById("motADeviner").innerHTML=temp;
+}
+
+function onload() {
+    lettreUtiliser="";
+    vie = 11;
+    i = Math.floor(Math.random()*listemots.length);
+    motactuel=listemots[i];
+    for (let j = 0; j < motactuel.length; j++) {
+        if (motactuel[j]!=" "){
+            motDeviner[j]="_";
+        }
+        else if (motactuel[j]=="'"||motactuel[j]=="-") {
+            motDeviner[j]==motactuel[j];
+        }
+        else{motDeviner[j]=".";}
+    }
+    afficher();
+}
+
+
+
+function validation() {
+    valeur= document.getElementById("lettreEntree").value;
+    if (valeur.length!=1) {
+       alert("Valeur impossible!!!");
+       return;
+    }
+    if (motactuel.includes(valeur)) {
+        //alert("Lettre valide");
+        for (let j = 0; j < motactuel.length; j++) {
+            if (valeur==motactuel[j]) {
+                motDeviner[j]=valeur;
+                }
+        }
+        //alert(motDeviner);
+        afficher();
+        victoire(motDeviner);
+    }
+    else{
+        vie--;
+        //document.getElementById("Vie").innerHTML=vie;
+        document.getElementById("imgVie").src="styles/perdu/"+(11-vie)+".png";
+        defaite();
+    }
+    lettreUtiliser=lettreUtiliser+valeur+" "
+    document.getElementById("lettreUtilisee").innerHTML=lettreUtiliser;
+    //document.body.style.backgroundColor="green";
+    document.getElementById("lettreEntree").value=null;
+}
+
+function victoire(mot) {
+    for (let i = 0; i < motactuel.length; i++) {
+        if (mot[i]!=motactuel[i]) {
+            return
+        }
+    }
+    alert("Mot Trouvé!! (Pour relancer recharge la page)");
+}
+function defaite() {
+    if (vie<=0) {
+        alert("Perdu!! (Pour relancer recharge la page)");
+    }
+}
